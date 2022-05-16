764097d7847787ceeaddbbc165292f0479fbdd8a
yufunz <yufu@example.com>
2022-05-16 13:49:29 +1200

    Add basic home layout

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,28 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    // Resize navbar when scrolling down
+    $(window).scroll(function () {
+        var fromTop = $(document).scrollTop();
+        if (fromTop > $('.navbar').height()) $('.navbar').addClass('minimized');
+        else $('.navbar').removeClass('minimized');
+    });
+}
 
-// Write your JavaScript code.
+$(function () {
+    var topoffset = 80; //variable for menu height
+
+    //Use smooth scrolling when clicking on navigation
+    $('.navbar-nav a').click(function () {
+        if (location.pathname.replace(/^\//, '') ===
+            this.pathname.replace(/^\//, '') &&
+            location.hostname === this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top - topoffset
+                }, 500);
+                return false;
+            } //target.length
+        } //click function
+    }); //smooth scrolling
+});
