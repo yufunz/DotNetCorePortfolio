7b875a9739192e64786686cd1134a48e23d8eeb1
yufunz <yufu@example.com>
2022-05-16 23:49:09 +1200

    Add about section

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -6,23 +6,3 @@
         else $('.navbar').removeClass('minimized');
     });
 });
-
-$(function () {
-    var topoffset = 80; //variable for menu height
-
-    //Use smooth scrolling when clicking on navigation
-    $('.navbar-nav a').click(function () {
-        if (location.pathname.replace(/^\//, '') ===
-            this.pathname.replace(/^\//, '') &&
-            location.hostname === this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html,body').animate({
-                    scrollTop: target.offset().top - topoffset
-                }, 500);
-                return false;
-            } //target.length
-        } //click function
-    }); //smooth scrolling
-});
