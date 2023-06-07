53caa7faa23645f5a87652faf4a773f5fdfccd06
Yuhei <yuhei.f@outlook.com>
2023-06-07 21:13:38 +1200

    Update Project model

---
+++ Migrations/20230607084907_UpdateProjectModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotNetCorePortfolio.Migrations
+{
+    public partial class UpdateProjectModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Url",
+                table: "Project",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Url",
+                table: "Project");
+        }
+    }
+}
--- Models/Project.cs
+++ Models/Project.cs
@@ -9,5 +9,6 @@ namespace DotNetCorePortfolio.Models
         public string Title { get; set; }
         public string Thumbnail { get; set; }
         public string Description { get; set; }
+        public string Url { get; set; } = string.Empty;
     }
 }
